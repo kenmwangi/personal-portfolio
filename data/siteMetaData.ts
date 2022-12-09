3fabebf031e1e825ff1b32f09eb3302b4c075e63
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-09 12:25:29 +0300

    refactor: refactored code variables

--- data/siteMetaData.ts
+++ data/siteMetaData.ts
@@ -1,5 +1,11 @@
 // Navigation links
+const name: string = 'Ken Mwangi';
+const date = new Date().getFullYear();
 
+// Social media
+const twitter: string = 'https://twitter.com/ken_cipher';
+const linkedin: string = `https://www.linkedin.com/in/kkmwangi/`;
+const github: string = `https://github.com/kenmwangi`;
 export const navLinks = [
   {
     id: '1',
@@ -20,3 +26,25 @@ export const navLinks = [
 
 // Logo
 export const Logo = 'KM.';
+
+// Footer
+export const footerCopyright = `${name} - ${date}`;
+
+// Social Links
+export const socialLinks = [
+  {
+    id: '1',
+    social: 'twitter',
+    link: `${twitter}`,
+  },
+  {
+    id: '2',
+    social: 'linkedin',
+    link: `${linkedin}`,
+  },
+  {
+    id: '3',
+    social: 'github',
+    link: `${github}`,
+  },
+];
