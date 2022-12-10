a653974e6541f672790be8297d0074ba5b90edf7
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-10 12:57:05 +0300

    refactor: improving Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,25 @@
-import { heroDesc, heroIntro, name } from '../data/siteMetaData';
+import Image from 'next/image';
+import { heroIntro, name } from '../data/siteMetaData';
 const Hero = () => {
   return (
-    <section className="mb-[170px] mt-[100px] flex items-center justify-between bg-slate-50">
-      <div className="">
-        {/* <img
+    <section className="mb-[170px] mt-[100px] flex items-center justify-center flex-col">
+      <div className="flex items-center gap-8 mb-8">
+        <Image
           src="/images/profile.jpg"
           alt="Profile"
-          className="object-cover rounded-full ring-1 ring-cyan-700 object-center"
-        /> */}
-
-        <h1 className="mt-10 text-[60px] font-thin text-slate-600  leading-[82px]">
-          Hello, I&apos;m <br />
-          <span className="tracking-widest text-slate-800">{name}</span>
-        </h1>
+          className="object-cover rounded-full object-center"
+          height={100}
+          width={100}
+        />
+        <div>
+          <h1 className="mt-10 text-xl md:text-3xl">{name}</h1>
+          <p className="text-sm text-slate-600">{heroIntro}</p>
+        </div>
       </div>
-      <div>
-        <p>{heroDesc}</p>
-        <h2>{heroIntro}</h2>
+      <div className="mt-8">
+        <h2 className="text-center text-3xl md:text-5xl font-extralight tracking-wider leading-8">
+          Developing purposeful <br /> & impactful user experiences
+        </h2>
       </div>
     </section>
   );
